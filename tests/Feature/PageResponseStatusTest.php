cc8959605b827ce609c1474accc557fa3093eb10
lucalana <lucalana13@gmail.com>
2025-04-17 15:29:14 -0300

    add comments

--- tests/Feature/PageResponseStatusTest.php
+++ tests/Feature/PageResponseStatusTest.php
@@ -3,17 +3,23 @@
 use function Pest\Laravel\get;
 
 test('testando código 200', function () {
+    //Arrange
+    //Act Assert
     get('/')->assertStatus(200);
 });
 
 test('testando código 404', function () {
+    //Arrange
+    //Act
     $response = get('/404');
-
+    //Assert
     $response->assertStatus(404);
 });
 
 test('testando código 403', function () {
+    //Arrange
+    //Act
     $response = get('/403');
-
+    //Assert
     $response->assertStatus(403);
 });
